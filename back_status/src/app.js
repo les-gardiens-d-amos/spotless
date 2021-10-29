d6d22d4db34dc32f715b76ccf10b797f6770c5ff
corvaisier <julien.corvaisier.br@gmail.com>
2021-10-29 13:40:33 +0200

    refacto : remove deprecated body-parse and replace with express.json

--- back_status/src/app.js
+++ back_status/src/app.js
@@ -1,12 +1,11 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import cors from 'cors';
 import requests from './requests/index.js';
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 app.use(requests);
