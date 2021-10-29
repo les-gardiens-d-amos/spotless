7b1356638c7db6b0418ae02c1d0582b7579f8f2d
corvaisier <julien.corvaisier.br@gmail.com>
2021-10-29 14:28:28 +0200

    refacto: update connection system to simplify

--- back_status/src/config/database.js
+++ back_status/src/config/database.js
@@ -1,28 +1,17 @@
-import mysql from "mysql2/promise";
+import mysql from "mysql2";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-async function connect() {
-  if (global.connection && global.connection.state !== "database disconnected") {
-    return global.connection;
-  }
-  try {
-    const connection = await mysql.createConnection({
-      database: process.env.DATABASE_NAME,
-      host: process.env.DATABASE_HOST,
-      password: process.env.DATABASE_PASS,
-      port: 3306,
-      user: process.env.DATABASE_USER,
-    });
+const pool = mysql.createPool({
+  database: process.env.DATABASE_NAME,
+  host: process.env.DATABASE_HOST,
+  password: process.env.DATABASE_PASS,
+  port: 3306,
+  user: process.env.DATABASE_USER,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
-    global.connection = connection;
-    console.log("database connected !");
-    return connection;
-  } catch (e) {
-    console.error("database fail to connect !", e);
-    return false;
-  }
-}
-
-export default connect;
+export default pool;
--- back_status/src/requests/status_api_status.js
+++ back_status/src/requests/status_api_status.js
@@ -1,16 +1,18 @@
 import express from "express";
-import connect from "../config/database.js";
+import pool from "../config/database.js";
 
 const status_api_status = express.Router();
 
 status_api_status.get("/status", (req, res) => {
-    connect().then((r) => {
-      if(r) {
-        res.json({ message: "connection to back: true, db: true" });
-      } else {
-        res.json({ message: "connection to back: true, db: false" });
-      }
-    });
+  pool.getConnection((e) => {
+    if (e) {
+        console.error("db connection fail", e);
+      res.json({ message: "connection to back: true, db: false" });
+    } else {
+      res.json({ message: "connection to back: true, db: true" });
+      connnection.release();
+    }
+  });
 });
 
 export default status_api_status;
