0ad1d1f73775c876d5d62b837b66f45c00e5dc30
corvaisier <julien.corvaisier.br@gmail.com>
2021-10-29 12:11:00 +0200

    add handle error to db connection

--- back_status/src/config/database.js
+++ back_status/src/config/database.js
@@ -1,24 +1,29 @@
-import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 async function connect() {
-    if (global.connection && global.connection.state !== 'disconnected') {
-      return global.connection;
-    }
- 
+  if (global.connection && global.connection.state !== "database disconnected") {
+    return global.connection;
+  }
+  try {
     const connection = await mysql.createConnection({
       database: process.env.DATABASE_NAME,
       host: process.env.DATABASE_HOST,
       password: process.env.DATABASE_PASS,
       port: 3306,
-      user: process.env.DATABASE_USER
+      user: process.env.DATABASE_USER,
     });
 
     global.connection = connection;
     console.log("database connected !");
     return connection;
+  } catch (e) {
+    console.log("database fail to connect !");
+    console.error(e);
+    return false;
+  }
 }
 
 export default connect;
